9d484434d8366c7e0b1794aaa17473bf2809df54
Dilkash931 <kashdil250@gmail.com>
2024-07-20 16:11:07 +0500

    pushing with automatic logout error

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('login');
+        }
+    }
+}
+
+
+// namespace App\Http\Middleware;
+
+// use Illuminate\Auth\Middleware\Authenticate as Middleware;
+// use Illuminate\Http\Request;
+
+// class Authenticate extends Middleware
+// {
+//     /**
+//      * Get the path the user should be redirected to when they are not authenticated.
+//      */
+//     protected function redirectTo(Request $request): ?string
+//     {
+//         return $request->expectsJson() ? null : route('login');
+//     }
+// }
